1fb8bff5f6d33b8e23b5811dc0591d9eaed5625f
Hju Kneyck Flores <hjucode@gmail.com>
2022-06-04 00:55:53 +0800

    chore: update example client

--- example/example_client.py
+++ example/example_client.py
@@ -3,59 +3,101 @@ import json
 import urllib.request
 from typing import Any, Callable
 from urllib.error import HTTPError
+from urllib.parse import urlencode
 
 BASE_URL = "http://127.0.0.1:8000"
 
 
-async def exec_as_aio(blocking_fn: Callable[..., Any], *args: Any):
+async def exec_as_aio(blocking_fn: Callable[..., Any], *args: Any) -> Any:
+    """Asyncronously run blocking functions.
+
+    Args:
+        blocking_fn (Callable[..., Any]): The blocking fn.
+
+    Returns:
+        Any: The return value/s of the blocking fn.
+    """
     loop = asyncio.get_running_loop()
     return await loop.run_in_executor(None, blocking_fn, *args)
 
 
-async def parse_response(response: Any) -> Any:
+async def parse_response(json_: Any) -> Any:
+    """Convert JSON object to a python data type.
+
+    Args:
+        response (Any): The JSON object to deserialize.
+
+    Returns:
+        Any: The deserialized JSON object.
+    """
     try:
-        return json.loads(response.read())
+        return json.loads(json_.read())
     except UnicodeDecodeError:
-        return response
+        return json_
 
 
-async def search(term: str) -> dict[str, Any]:
-    resp = await exec_as_aio(urllib.request.urlopen, f"{BASE_URL}/search?term={term}")
+async def search(query: str) -> dict[str, Any]:
+    """Make a request to the /search endpoint.
+
+    Args:
+        query (str): The search term/query.
+
+    Returns:
+        dict[str, Any]: The search result.
+    """
+    resp = await exec_as_aio(urllib.request.urlopen, f"{BASE_URL}/search?{query}")
     return await parse_response(resp)
 
 
-async def save(video: str) -> dict[str, Any]:
-    resp = await exec_as_aio(urllib.request.urlopen, f"{BASE_URL}/save?video={video}")
+async def convert(query: str) -> dict[str, Any]:
+    """Make a request to convert a video to audio.
+
+    Args:
+        query (str): The search term/query or video url.
+
+    Returns:
+        dict[str, Any]: A JSON response.
+    """
+    resp = await exec_as_aio(urllib.request.urlopen, f"{BASE_URL}/convert?{query}")
     return await parse_response(resp)
 
 
-async def download(ticket: str) -> None:
+async def save(ticket: str) -> None:
+    """Save the audio file using assigned ticket.
+
+    Args:
+        ticket (str): The value of the ticket key in the JSON response.
+    """
     try:
-        await exec_as_aio(
-            urllib.request.urlopen, f"{BASE_URL}/download?ticket={ticket}"
+        filedata = await exec_as_aio(
+            urllib.request.urlopen, f"{BASE_URL}/save?{ticket}"
         )
+        datatowrite = filedata.read()
+        with open("audio_file.m4a", "wb") as f:
+            f.write(datatowrite)
     except HTTPError as http_err:
         if http_err.code == 409:
             # Clients are rate limited to 5 requests per minute by default
             # Too many requests will result in a 429 response
             await asyncio.sleep(13)
-            await download(ticket)
+            await save(ticket)
     else:
         await exec_as_aio(
             urllib.request.urlretrieve,
-            f"{BASE_URL}/download?ticket={ticket}",
+            f"{BASE_URL}/download?{ticket}",
             "audio_file.m4a",
         )
 
 
-async def main(video: str) -> None:
-    if "http" not in video:
-        video = "+".join(video.split(" "))
+async def main(query: str) -> None:
+    query = urlencode({"query": query}) if "http" not in query else query
 
-    result = await search(video)
+    result = await search(query)
     print(result)
-    response = await save(video)
-    await download(response["ticket"])
+
+    response = await convert(query)
+    print(response)
+    await save(f"""ticket={response["ticket"]}""")
 
 
 asyncio.run(main("when the comedic timing is just perfect~"))
